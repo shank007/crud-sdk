d1193c91fcbe727a03ab8f30c7278779196d46d3
lenovo <giri@cateina.com>
2018-09-10 18:29:25 +0530

    CRUD

--- dist/engine.js
+++ dist/engine.js
@@ -3,5 +3,5 @@ var engine = require('../app/crud');
 module.exports.create = engine.create
 module.exports.readById = engine.readById
 module.exports.readByCondition = engine.readByCondition
-module.exports.update = engine.updateData
-module.exports.delete = engine.deleteData
+module.exports.update = engine.update
+module.exports.delete = engine.delete
