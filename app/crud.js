7b9f2372c09cf15fb931f784cba649b1da475a12
lenovo <giri@cateina.com>
2018-09-20 20:52:38 +0530

    Features

--- app/crud.js
+++ app/crud.js
@@ -222,7 +222,7 @@ var aggregate = function (connectionString, dbName, collectionName, condition, a
         // var queryData = JSON.parse(condition);
         db.bind(collectionName);
 
-        db.collection(collectionName).find(condition).aggregate(aggregateCondition).toArray(function (err, result) {
+        db.collection(collectionName).aggregate(aggregateCondition, function (err, result) {
             if (err) {
                 return callback(err, result);
             }
@@ -252,7 +252,7 @@ var limit = function (connectionString, dbName, collectionName, condition, limit
         // var queryData = JSON.parse(condition);
         db.bind(collectionName);
 
-        db.collection(collectionName).aggregate(aggregateCondition, function (err, result) {
+        db.collection(collectionName).find(condition).limit(limit).toArray(function (err, result) {
             if (err) {
                 return callback(err, result);
             }
